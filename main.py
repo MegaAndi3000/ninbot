352e9458fc2964ed90f8cc38dc9c7db006a63dd1
MegaAndi3000 <75537640+MegaAndi3000@users.noreply.github.com>
2024-08-09 15:40:21 +0200

    [#6] added main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import threading
+from subprocess import call
+
+files = ['log_bot.py',
+         'poll_bot.py',
+         'shitcoin_bot.py']
+
+def func(script):
+
+    call(['python', script])
+
+for script in files:
+
+    x = threading.Thread(target = func, args = (script,))
+    x.start()
