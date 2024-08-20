9fec372fcf41a1fc94ff7c04255ee030dd3842a5
MegaAndi3000 <75537640+MegaAndi3000@users.noreply.github.com>
2024-08-20 18:24:46 +0200

    added zr command

--- quote_ranking.py
+++ quote_ranking.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from funcs import get_ids, get_id_to_nick
+from funcs import get_ids, get_id_to_nick, sort_dic_value
 
 load_dotenv()
 TOKEN = os.getenv('NINBOT_TOKEN')
@@ -11,16 +11,22 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='.', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_ready():
+async def update():
     
-    print('READY')
-       
+    global author_list
+    global victim_list
+    
     id_list = get_ids()
     id_to_nick = await get_id_to_nick(bot)
     id_to_nick[753298872134271236] = 'Cathi'
+    
+    alias_list = {'Andreas': ['Andi', 'Bagehorn', 'MegaderAndi'],
+                  'Templum': ['Nazi-Kon', 'Konrad T'],
+                  'Tobi': ['Tobias'],
+                  'Rübe': ['Konrad R'],
+                  'Finn': []}
     
     channel = bot.get_channel(id_list['zitate'])
     message_list = []
@@ -41,25 +47,6 @@ async def on_ready():
 
     del message_list[-9:]
     message_list = message_list[::-1]
-    
-    print('Done')
-    
-    # unique_victims = []
-    
-    # for quote in message_list:
-        
-    #     if quote['victim'] not in unique_victims:
-            
-    #         unique_victims.append(quote['victim'])
-            
-    # print(unique_victims)
-
-    alias_list = {'Andreas': ['Andi', 'Bagehorn', 'MegaderAndi'],
-                  'Templum': ['Konrad', 'Nazi-Kon'],
-                  'Tobi': ['Tobias'],
-                  'Rübe': [],
-                  'Finn': []}
-    
     quotes_cleaned = []
     
     for quote in message_list:
@@ -93,11 +80,7 @@ async def on_ready():
             author_list[quote['author']]['count'] += 1
             author_list[quote['author']]['likes'] += quote['likes']
             author_list[quote['author']]['dislikes'] += quote['dislikes']
-            
-    # for author, stats in author_list.items():
-        
-    #     print(f'{author}: {stats['count']} Zitate, {stats['likes']} Likes, {stats['dislikes']} Dislikes, {stats['likes']/stats['count']:.2f} DLikes, {stats['dislikes']/stats['count']:.2f} DDislikes')
-
+   
     victim_list = {}
     
     for quote in quotes_cleaned:
@@ -112,36 +95,46 @@ async def on_ready():
             victim_list[quote['victim']]['likes'] += quote['likes']
             victim_list[quote['victim']]['dislikes'] += quote['dislikes']
 
-    for victim, stats in victim_list.items():
-
-        print(f'{victim}: {stats['count']} Zitate, {stats['likes']} Likes, {stats['dislikes']} Dislikes, {stats['likes']/stats['count']:.2f} DLikes, {stats['dislikes']/stats['count']:.2f} DDislikes')
-
-    max_likes_list = []
-    max_likes = 0
+@bot.event
+async def on_ready():
     
-    max_dislikes_list = []
-    max_dislikes = 0
+    await update()
+    print('Ready')
 
-    for quote in message_list:
+@bot.command(name='zr')
+async def quote_count_ranking(ctx):
+    
+    id_list = get_ids()
+    
+    if ctx.channel.id != id_list['bot-spam']:
         
-        if quote['likes'] > max_likes:
-            
-            max_likes = quote['likes']
-            max_likes_list = [quote]
-            
-        elif quote['likes'] == max_likes:
-            
-            max_likes_list.append(quote)
-            
-        if quote['dislikes'] > max_dislikes:
-            
-            max_dislikes = quote['dislikes']
-            max_dislikes_list = [quote]
-            
-        elif quote['dislikes'] == max_dislikes:
-            
-            max_dislikes_list.append(quote)
-            
-    # print(f'LIKES\n\n{max_likes_list}\n\nDISLIKES\n\n{max_dislikes_list}')
+        pass
+    
+    else:
+    
+        print('HERE')
+    
+        authors = sort_dic_value(author_list, 'count')
+        victims = sort_dic_value(victim_list, 'count')
 
+        response = '=== Autoren ===\n'
+        i = 0
+        
+        for author, stats in authors.items():
+            
+            if stats['count'] >= 5:
+            
+                response += f'\n{i}. {author} mit {stats['count']} Zitaten'
+                i += 1
+            
+        response += '\n\n=== "Opfer" ===\n'
+        i = 0
+        
+        for victim, stats in victims.items():
+            
+            response += f'\n{i}. {victim} mit {stats['count']} Zitaten'
+            i += 1
+            
+        await ctx.send(response)
+        
 bot.run(TOKEN)
