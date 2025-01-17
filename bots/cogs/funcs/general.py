72308821c26137f489e05802f9902d5deae99910
MegaAndi3000 <megaandi3000@gmail.com>
2025-01-17 13:43:27 +0100

    cogs: Add funcs

---
+++ bots/cogs/funcs/general.py
@@ -0,0 +1,156 @@
+def get_ids():
+    """Reads the ids of channels und users from '.ids'.
+
+    Returns:
+        dict: directory, which maps name (str) to id (int)
+    """
+    
+    id_dict = {}
+    
+    with open('.ids', 'r') as file:
+        
+        lines = file.readlines()
+        
+        for line in lines:
+        
+            if len(line) > 2:
+            
+                split = line.split(' = ')    
+                id_dict[split[0]] = int(split[1])
+                
+    return id_dict
+
+async def get_id_to_nick(bot):
+    """Generates a directory, which maps id to nick (or id to name, if nick doesn't exist).
+
+    Args:
+        bot (bot): a discord.py bot client
+
+    Returns:
+        dict: directory, which maps id (int) to nick (str)
+    """
+
+    id_to_nick = {}
+
+    for guild in bot.guilds:
+            
+        async for member in guild.fetch_members():
+        
+            if member.nick:
+                
+                id_to_nick[member.id] = member.nick  
+
+            else:
+                
+                id_to_nick[member.id] = member.name
+
+    return id_to_nick
+
+async def get_nick_to_id(bot):
+    """Generates a directory, which maps nick to id (or name to id, if nick doesn't exist).
+
+    Args:
+        bot (bot): a discord.py bot client
+
+    Returns:
+        dict: directory, which maps nick (str) to id (int)
+    """
+    
+    nick_to_id = {}
+    
+    for guild in bot.guilds:
+                   
+        async for member in guild.fetch_members():
+            
+            if member.nick:
+                
+                nick_to_id[member.nick] = member.id
+                
+            else:
+                
+                nick_to_id[member.name] = member.id
+                
+    return nick_to_id
+
+def set_string_length(string:str, length:int):
+    """Extends or shortens a given string to a given length.
+
+    Args:
+        string (str): string, which is to be edited
+        length (int): the resulting length
+
+    Returns:
+        str: edited string with given length
+    """
+    
+    if len(string) > length:
+        
+        new_string = string[:length]
+        
+    else:
+        
+        new_string = string + " " * (length - len(string))    
+    
+    return new_string
+
+def sort_dic(dictionary:dict):
+    """Sorts a directory by its values
+
+    Args:
+        dictionary (dict): directory, which is to be sorted
+
+    Returns:
+        dict: sorted dictory
+    """
+    
+    new_dic = {}
+    
+    values = list(dictionary.values())
+    values.sort(reverse=True)
+    
+    for value in values:
+        
+        for key in dictionary:
+            
+            if dictionary[key] == value:
+                
+                if key not in new_dic:
+                    
+                    new_dic[key] = value
+                    
+    return new_dic
+
+def sort_dic_value(dictionary:dict, sort_value:str):
+    """Sorts a dictionary, which contains dictionaries by values of the latter.
+
+    Args:
+        dictionary (dict): dictionary, which is to be sorted
+        sort_value (str): name of the value, by which to sort
+
+    Returns:
+        dict: sorted dictionary
+    """
+    
+    new_dic = {}
+    
+    dictionaries = list(dictionary.values())
+    values = []
+    
+    for dic in dictionaries:
+        
+        values.append(dic[sort_value])
+    
+    values.sort(reverse=True)
+    
+    for value in values:
+        
+        for key, dic in dictionary.items():
+            
+            if dic[sort_value] == value:
+                
+                if key not in new_dic:
+                    
+                    new_dic[key] = dic
+                    
+    return new_dic
+    
---
+++ bots/cogs/funcs/stock_market.py
@@ -0,0 +1,40 @@
+import os
+from random import random
+from math import tan
+from math import pi
+from dotenv import load_dotenv
+
+def get_factor(x:float):
+    """Calculates the factor to multiply the current price with based on a seed.
+
+    Args:
+        x (float): seed (NOTE: The seed should be in [0.5, 1].)
+
+    Returns:
+        float: The calculated factor > 0.
+    """
+    load_dotenv()
+    return (tan(pi*x + pi/2) + 1)**(1/int(os.getenv('STOCK_STABILITY')))
+
+def stock_update(current_price:float):
+    """Updates the price of a stock.
+
+    Args:
+        current_price (float): The current price of the stock which is to be updated.
+
+    Returns:
+        float: The new price of the stock
+    """
+    factor_seed = random()
+    
+    if factor_seed >= 0.5:
+        
+        factor = get_factor(factor_seed)
+        
+    else:
+        
+        factor = 1/get_factor(-factor_seed)
+    
+    new_price = current_price * factor
+    
+    return new_price
