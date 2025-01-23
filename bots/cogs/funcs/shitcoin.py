e857a64d809fa3bea24fbec9ab62296f5ac0162e
MegaAndi3000 <megaandi3000@gmail.com>
2025-01-23 16:27:54 +0100

    shitcoin: Add missing all_time_top_list update

--- bots/cogs/funcs/shitcoin.py
+++ bots/cogs/funcs/shitcoin.py
@@ -2,7 +2,11 @@ import json
 from dotenv import load_dotenv
 
 def file_update(data):
-                        
+
+    for user, balance in data["shit_coin_list"].items():
+        if balance > data["all_time_top_list"][user]:
+            data["all_time_top_list"][user] = balance
+
     with open("Data/shitcoin.json", "w") as file:
         json.dump(data, file)
     
